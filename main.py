ad605597a82cec174ecee6ce55481ac980637c92
JulietZ <Juliet@Junyes-Air.home>
2014-12-13 22:08:16 -0500

    mainpage finish

--- main.py
+++ main.py
@@ -18,94 +18,67 @@
 import cgi
 import urllib
 from google.appengine.api import users
-from google.appengine.ext import db
+from google.appengine.ext import ndb
 import webapp2
+import os
+import jinja2
+import datetime
 
-class User(db.Model):
-	userid = db.StringProperty(required=True)
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
-class Questions(db.Model):
-	questionTime = db.DateProperty(auto_now_add=True)
-	questionUser = db.StringProperty(required=True)
-	questionName = db.StringProperty(required=True)
-	questionContent = db.StringProperty(required=True)
-	#questionTag = #list, tag
+def site_key():
+    """Constructs a website key for All questions."""
+    return ndb.Key('Site', 'site')
 
-class Answers(db.Model):
-	questionName = db.StringProperty(required=True)
-	questionUser = db.StringProperty(required=True)
-	answerName = db.StringProperty(required=True)
-	answerUser = db.StringProperty(required=True)
-	answerContent = db.StringProperty(required=True)
-	answerTime = db.TimeProperty(auto_now_add=True)
+class Questions(ndb.Model):
+    """Models an individual Question entry """
+    author = ndb.UserProperty()
+    content = ndb.StringProperty(indexed=False)
+    createDate = ndb.DateTimeProperty(auto_now_add=True)
+    title = ndb.StringProperty(indexed=False)
+    modifyDate = ndb.DateTimeProperty()
+    tag = ndb.StringProperty(repeated=True)
+    voteScore = ndb.IntegerProperty()
+    
+class Answers(ndb.Model):
+    """Models an individual Answer entry """
+    author = ndb.UserProperty()
+    content = ndb.StringProperty(indexed=False)
+    createDate = ndb.DateTimeProperty(auto_now_add=True)
+    modifyDate = ndb.DateTimeProperty()
+    voteScore = ndb.IntegerProperty()
+    qAuthor = ndb.UserProperty()
+    qTitle = ndb.StringProperty(indexed=False)
 
-class Votes(db.Model):
-	questionName = db.StringProperty(required=True)
-	questionUser = db.StringProperty(required=True)
-	answerName = db.StringProperty()
-	answerUser = db.StringProperty()
-	voteUser = db.StringProperty(required=True)
-	voteTime = db.DateProperty(auto_now_add=True)
-	voteType = db.StringProperty(required=True, choices=set(["up", "down"]))
+class Votes(ndb.Model):
+    author = ndb.UserProperty(required=True)
+    createDate = ndb.DateTimeProperty(auto_now_add=True)
+    voteType = ndb.StringProperty(choices=set(["up", "down"]))
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        existing = db.GqlQuery("SELECT * FROM User WHERE userid = :1",user)
-        existing = User.all()
-        existing.filter("userid =", user)
-        self.response.out.write('<html><body>')
         if user:
-        	if existing == "":
-        		newuser = User(userid = user)
-        		newuser.put()
-        	self.response.out.write('Welcome, %s! (<a href="%s">sign out</a>)' %
-                        (user.nickname(), users.create_logout_url('/')))
-        	self.response.out.write('</br><a href="/question">Create New Question</a>')
-        	newquestion = Questions(questionName="test question",questionUser=user.nickname(),questionContent="Test now")
-        	newquestion.put()
-            
+            url = users.create_logout_url(self.request.uri)
+
         else:
-            self.response.out.write('<a href="%s">Sign in or register</a>.' %
-                        users.create_login_url('/'))
+            url = users.create_login_url(self.request.uri)
+        questions_query = Questions.query(
+           ancestor=site_key()).order(-Questions.createDate)
+        questions = questions_query.fetch(10)
+        template_values = {
+            'user': users.get_current_user(),
+            'questions': questions,
+            'url': url
+        }
+        template = JINJA_ENVIRONMENT.get_template('mainpage.html')
+        self.response.write(template.render(template_values))
 
-        questions=db.GqlQuery("SELECT * FROM Questions ORDER BY questionTime DESC")
-        self.response.out.write('<table style="width:100%">')
-        self.response.out.write('<tr><td>question time</td><td>User</td><td>Title</td><td>Content</td></tr>')
-        for question in questions:
-        	self.response.out.write('<tr><td>%s</td><td>%s</td><td><a href="/%s">%s</a></td><td>%s</td></tr>' %
-        		(question.questionTime,question.questionUser,question.questionName,question.questionName,question.questionContent))
-        self.response.out.write('</table>')
-        self.response.out.write('</html></body>')
-
-Create_Question_PAGE_FOOTER_TEMPLATE = """\
-    <form action="/newquestion" method="post">
-      <div>Question Name: <textarea name="name"></textarea></div>
-      <div>Your Question: <textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Submit Question"></div>
-    </form>
-  </body>
-</html>
-"""
-
-class Question(webapp2.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
-        self.response.out.write('<html><body>')
-        if user:
-        	self.response.write(Create_Question_PAGE_FOOTER_TEMPLATE %
-                            (sign_query_params, cgi.escape(guestbook_name),
-                             url, url_linktext))
-            
-        else:
-            self.response.out.write('<a href="%s">Sign in or register</a>.' %
-                        users.create_login_url('/'))
-
-        self.response.out.write('</html></body>')
-
-app = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/question', Question),
-    ('/newquestion',MainPage),
+application = webapp2.WSGIApplication([
+    ('/', MainPage)
 ], debug=True)
+
