22aa52ea2e2607589119109bef6eacdcfa4499e9
JulietZ <Juliet@Junyes-Air.home>
2014-12-15 01:23:15 -0500

    fix little bug

--- main.py
+++ main.py
@@ -78,20 +78,23 @@ class MainPage(webapp2.RequestHandler):
         pagesize=10
         nextpage=False
         previouspage=False
-        if self.request.get('pageno'):
-            pageno=int(self.request.get('pageno'))
-        if self.request.get('nextpage'):
-            pageno=pageno+1
-        if self.request.get('previouspage'):
-            pageno=pageno-1
-        if pageno>1:
-            previouspage=True
-        if len(lenquestion)>(pagesize*pageno):
-            nextpage=True
-            temp=pagesize
+        if len(lenquestion)>0:
+            if self.request.get('pageno'):
+                pageno=int(self.request.get('pageno'))
+            if self.request.get('nextpage'):
+                pageno=pageno+1
+            if self.request.get('previouspage'):
+                pageno=pageno-1
+            if pageno>1:
+                previouspage=True
+            if len(lenquestion)>(pagesize*pageno):
+                nextpage=True
+                temp=pagesize
+            else:
+                temp=len(lenquestion)-(pagesize*(pageno-1))
+            questions = questions_query.fetch(temp,offset=(pageno-1)*pagesize)
         else:
-            temp=len(lenquestion)-(pagesize*(pageno-1))
-        questions = questions_query.fetch(temp,offset=(pageno-1)*pagesize)
+            questions = lenquestion
         
         template_values = {
             'user': users.get_current_user(),
